3b4543a26f405b9255e94a0cdc21ee93226741c6
Joe Cheng <joe@rstudio.org>
2012-12-01 02:18:13 -0800

    Implement worker registry
    Keeps track of what app workers are running, and can return existing
    app workers or create new ones.

---
+++ test/test-worker-registry.js
@@ -0,0 +1,19 @@
+var util = require('util');
+require('../lib/core/log')
+var AppSpec = require('../lib/worker/app-spec');
+var WorkerRegistry = require('../lib/worker/worker-registry');
+
+var registry = new WorkerRegistry();
+registry.getWorker_p(new AppSpec(
+  '/Users/jcheng/ShinyApps/diamonds', 'jcheng', null, {}))
+.then(function(info) {
+  logger.info(util.inspect(info));
+})
+.done();
+
+registry.getWorker_p(new AppSpec(
+  '/Users/jcheng/ShinyApps/diamonds', 'jcheng', null, {}))
+.then(function(info) {
+  logger.info(util.inspect(info));
+})
+.done();
