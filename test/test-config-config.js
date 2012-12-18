73decaaaf2daaa38676c9f857ecea20165aecb6a
Joe Cheng <joe@rstudio.org>
2012-12-18 15:23:36 -0800

    Further implement validation/schema

--- test/test-config-config.js
+++ test/test-config-config.js
@@ -1,13 +1,19 @@
 var assert = require('assert');
+var fs = require('fs');
 var path = require('path');
 var util = require('util');
 var config = require('../lib/config/config');
+var schema = require('../lib/config/schema');
 
-var configData = config.parseConfigData('\
+var schemaPath = path.join(__dirname, '../lib/config/shiny-server-rules.config');
+
+var configData = config.parse('\
 runas nobody;\n\
+max_workers 10;\n\
 server {\n\
   location / {\n\
-    runas jcheng;\n\
+    runas jcheng jcheng;\n\
+    runas jcheng; # valid because second arg is optional\n\
     appdir ~/myshinyapp;\n\
   }\n\
 }');
@@ -17,5 +23,22 @@ assert.deepEqual(configData.getOne('runas').args, ['nobody']);
 assert.equal(configData.getOne('server').getOne('location').getValue('runas'), 'jcheng');
 assert.equal(configData.search('location')[0].getValue('runas'), 'jcheng');
 
-var validationRules = config.readSync(path.join(__dirname, '../lib/config/shiny-server-rules.config'));
-config.validate(validationRules, configData);
+var validationRules = config.parse(fs.readFileSync(schemaPath, 'utf8'));
+schema.applySchema(configData, validationRules);
+
+var configGood = config.readSync(path.join(__dirname, 'config/good.config'), schemaPath);
+assertBad('bad1', schemaPath, /Unknown directive/);
+assertBad('bad2', schemaPath, /too few arguments/);
+assertBad('bad3', schemaPath, /too many arguments/);
+assertBad('good', path.join(__dirname, 'config/schemaBad1.config'), /Missing "at"/);
+assertBad('good', path.join(__dirname, 'config/schemaBad2.config'), /Unknown type "Number"/);
+
+function assertBad(file, schemaPath, regex) {
+  try {
+    config.readSync(path.join(__dirname, 'config', file + '.config'), schemaPath);
+    assert(false, file + " passed parse when it should've failed");
+  } catch(err) {
+    if (!regex.test(err.message))
+      throw err;
+  }
+}
