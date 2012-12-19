186d6a2e8ce72f54eb3e1d098e1dfa7c87415a37
Joe Cheng <joe@rstudio.org>
2012-12-18 17:07:56 -0800

    Separate sockjs and http proxies

--- test/test-proxy.js
+++ test/test-proxy.js
@@ -1,10 +1,15 @@
-var proxy = require('../lib/proxy/proxy');
+var proxy_http = require('../lib/proxy/http');
+var proxy_sockjs = require('../lib/proxy/sockjs');
 var router = require('../lib/router/router')
 var WorkerRegistry = require('../lib/worker/worker-registry');
 
-var shinyProxy = new proxy.ShinyProxy(
-  new router.AutouserRouter(),
-  new WorkerRegistry(),
+var rout = new router.AutouserRouter();
+var workers = new WorkerRegistry();
+
+var shinyProxy = new proxy_http.ShinyProxy(
+  rout,
+  workers,
   {}
 );
+proxy_sockjs.createServer(rout, workers).installHandlers(shinyProxy.httpServer);
 shinyProxy.httpServer.listen(8001);
