f652663ab07d9238973277368c697e6e37805adc
Joe Cheng <joe@rstudio.org>
2013-01-07 10:28:12 -0800

    Copyright headers

--- src/posix.cc
+++ src/posix.cc
@@ -1,3 +1,15 @@
+/*
+ * posix.cc
+ *
+ * Copyright (C) 2009-13 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
 #include <node.h>
 #include <v8.h>
 #include <stdlib.h>
