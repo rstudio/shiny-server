6824aaf206fabaa4e984563a9dd9a2a5dd60a4b0
trestletech <jeff.allen@trestletechnology.net>
2013-06-13 11:19:50 -0700

    Added agpl headers to the tests.

--- test/simple-scheduler.js
+++ test/simple-scheduler.js
@@ -1,3 +1,16 @@
+/*
+ * test/simple-scheduler.js
+ *
+ * Copyright (C) 2009-13 by RStudio, Inc.
+ *
+ * This program is licensed to you under the terms of version 3 of the
+ * GNU Affero General Public License. This program is distributed WITHOUT
+ * ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+ * AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+ *
+ */
+
 var SimpleScheduler = require('../lib/scheduler/simple-scheduler.js');
 var AppSpec = require('../lib/worker/app-spec.js');
 var sinon = require('sinon');
