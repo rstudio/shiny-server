268c60e7a394667e97fd41fe384a8100acc6f467
trestletech <jeff.allen@trestletechnology.net>
2013-07-17 13:52:49 -0700

    Implemented test for the initial creation of a scheduler.

--- test/scheduler-registry.js
+++ test/scheduler-registry.js
@@ -11,9 +11,34 @@
  *
  */
 
+var Scheduler = require('../lib/scheduler/scheduler');
+var SimpleScheduler = require('../lib/scheduler/simple-scheduler');
+var SchedulerRegistry = require('../lib/scheduler/scheduler-registry');
+var AppSpec = require('../lib/worker/app-spec');
+var sinon = require('sinon');
+var Q = require('q');
+var _ = require('underscore');
+
+var appSpec = new AppSpec("/var/shiny-www/01_hello/", "jeff", "", "/tmp", {})
+var socketDir = "/tmp/shiny-session/";
+
 describe('SchedulerRegistry', function(){
   describe('#getWorker_p', function(){
-    it('Creates a new scheduler on initial request.'),
+    it('Creates a new scheduler on initial request.', function(done){
+      var schedReg = new SchedulerRegistry();
+      schedReg.setSocketDir(socketDir);
+
+      _.size(schedReg.$schedulers).should.equal(0);
+      schedReg.getWorker_p(appSpec)
+      .then(function(wh){
+        _.keys(schedReg.$schedulers).should.includeEql(appSpec.getKey());
+        (schedReg.$schedulers[appSpec.getKey()] instanceof SimpleScheduler).should.be.true;
+        return (wh);
+      })
+      .then(function(wh){ wh.kill('SIGABRT'); return(wh.exitPromise); })
+      .then(function() {})
+      .then(done, done).done();
+    }),
     it('Doesn\'t create a new scheduler for a repeat request.'),
     it('Creates a new scheduler for a new AppSpec'),
     it('Properly considers local app config when creating a new scheduler.')
