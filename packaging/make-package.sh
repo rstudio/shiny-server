2ec3ba6ed092a51e70c6185e6191375c13b6d587
Joe Cheng <joe@rstudio.com>
2013-08-13 22:18:55 -0700

    Use python26 on CentOS for node build

--- packaging/make-package.sh
+++ packaging/make-package.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+if [ "$PYTHON" == "" ]
+then
+	# python26 is required for RedHat/CentOS 5
+	if [ -x /usr/bin/python26 ]
+	then
+		PYTHON=python26
+	else
+		PYTHON=python
+	fi
+fi
+
 if [ "$1" != "DEB" -a "$1" != "RPM" ]
 then
 	echo "Usage: make-package.sh [DEB|RPM]"
@@ -13,8 +24,8 @@ cd "$DIR"
 DIR=`pwd`
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/opt ../..
+cmake -DCMAKE_INSTALL_PREFIX=/opt -DPYTHON="$PYTHON" ../..
 make
-(cd ../.. && bin/npm install)
+(cd ../.. && bin/npm --python="$PYTHON" install)
 cpack -G "$1"
 
