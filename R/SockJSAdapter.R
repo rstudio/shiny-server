4f34e9db6c0a41a7640fd64967d1e4131896c1a9
Joe Cheng <joe@rstudio.com>
2013-08-17 00:04:12 -0700

    Bundle sockjs client library (fixes issue #11)

--- R/SockJSAdapter.R
+++ R/SockJSAdapter.R
@@ -45,7 +45,7 @@ local({
    }
 
    inject <- paste(
-      tags$script(src='//d1fxtkz8shb9d2.cloudfront.net/sockjs-0.3.min.js'),
+      tags$script(src='__assets__/sockjs-0.3.min.js'),
       tags$script(
         HTML(
           paste(
