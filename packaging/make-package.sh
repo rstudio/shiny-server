431aee1f4fb1c959aa852c6a5d1312a47c224ee3
Joe Cheng <joe@rstudio.com>
2013-08-18 00:17:51 -0700

    Add package building script

---
+++ packaging/make-package.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$1" != "DEB" -a "$1" != "RPM" ]
+then
+	echo "Usage: make-package.sh [DEB|RPM]"
+fi
+
+DIR=`dirname $0`
+cd "$DIR"
+DIR=`pwd`
+mkdir -p build
+cd build
+cmake --prefix=/opt/shiny-server ../..
+make
+(cd ../.. && bin/npm install)
+cpack -G "$1"
+
