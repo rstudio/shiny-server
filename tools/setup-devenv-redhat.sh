9a7d334fb4fac334e8541cfbe88528bfe4be6992
Joe Cheng <joe@rstudio.com>
2013-08-21 10:53:36 -0700

    Fix setup-devenv-redhat script

--- tools/setup-devenv-redhat.sh
+++ tools/setup-devenv-redhat.sh
@@ -4,6 +4,13 @@ set -e
 
 cd "$(dirname $0)"
 
-sudo yum update make gcc gcc-c++ git python26 openssl-devel cmake28
+sudo yum update make gcc gcc-c++ git python openssl-devel
+
+if ! hash cmake 2>/dev/null; then
+	wget http://www.cmake.org/files/v2.8/cmake-2.8.11.2.tar.gz
+	tar xzf cmake-2.8.11.2.tar.gz
+	(cd cmake-2.8.11.2 && ./configure && make && sudo make install)
+	rm -rf cmake-2.8.11.2
+fi
 
 ./_setup-devenv-common.sh
