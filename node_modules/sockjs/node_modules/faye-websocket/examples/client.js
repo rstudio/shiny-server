ea027bc5b894085fe1762866b6627b6606122dad
Joe Cheng <joe@rstudio.com>
2014-06-05 23:12:31 -0700

    Upgrade sockjs to patched version
    Includes b88f9ea, which fixes sockjs/sockjs-node#143. This bug was
    causing JJ's ggvis-demo Shiny doc to grey out after about 45 seconds
    of inactivity.

--- node_modules/sockjs/node_modules/faye-websocket/examples/client.js
+++ node_modules/sockjs/node_modules/faye-websocket/examples/client.js
@@ -2,7 +2,9 @@ var WebSocket = require('../lib/faye/websocket'),
     port      = process.argv[2] || 7000,
     secure    = process.argv[3] === 'ssl',
     scheme    = secure ? 'wss' : 'ws',
-    ws        = new WebSocket.Client(scheme + '://localhost:' + port + '/');
+    url       = scheme + '://localhost:' + port + '/',
+    headers   = {Origin: 'http://faye.jcoglan.com'},
+    ws        = new WebSocket.Client(url, null, {headers: headers});
 
 console.log('Connecting to ' + ws.url);
 
