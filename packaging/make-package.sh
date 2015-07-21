9b7a3061302ef05b4e877f6ed04be471e3c079a9
Joe Cheng <joe@rstudio.org>
2015-07-21 16:37:51 -0700

    Attempt to fool RedHat5 to use the right python

--- packaging/make-package.sh
+++ packaging/make-package.sh
@@ -16,6 +16,13 @@ then
 	fi
 fi
 
+if [ "$PYTHON" != "python" ]
+then
+	mkdir -p $(realpath .)/fake_python
+	ln -s $(which $PYTHON) ./fake_python/python
+	export PATH=$(realpath .)/fake_python:$PATH
+fi
+
 GENERATOR="$1"
 if [ "$GENERATOR" == "" ]
 then
