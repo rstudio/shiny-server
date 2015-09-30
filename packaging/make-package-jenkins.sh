00943fb076c6f949c46a42655b1c1a4af9a071e8
Joe Cheng <joe@rstudio.org>
2015-09-30 14:02:07 -0700

    make-package-jenkins.sh wraps make-package.sh for centos5/6

---
+++ packaging/make-package-jenkins.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This is needed for CentOS5/6 Jenkins workers to bootstrap the gcc-4.8 toolchain.
+
+cd "$(dirname $0)"
+
+if (scl -l | grep -q devtoolset-2);
+then
+	scl enable devtoolset-2 ./make-package.sh "$@"
+else
+	./make-package.sh "$@"
+fi
+
