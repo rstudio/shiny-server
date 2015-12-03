73b46c8ec7686566d0d90e462a271dbd8efcce7f
trestletech <jeff.allen@trestletechnology.net>
2015-12-03 16:01:57 -0600

    Install NFS after creating /home/shiny to get around a warning about the dir not existing.
    Not sure if that caused actual problems too, or not.

--- vagrant/nfs/provision-nfs.sh
+++ vagrant/nfs/provision-nfs.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# install NFS server and export user home directories
-apt-get install -y nfs-kernel-server
-echo "/home/shiny   *(rw,sync,root_squash)" >> /etc/exports
-service nfs-kernel-server start
-
 # Create Shiny user
 useradd -r -m shiny
 mkdir /home/shiny/ShinyApps/
 chown -R shiny:shiny /home/shiny
 chmod 700 -R /home/shiny
+
+# install NFS server and export user home directories
+apt-get install -y nfs-kernel-server
+echo "/home/shiny   *(rw,sync,root_squash)" >> /etc/exports
+service nfs-kernel-server start
 
 # Install apps in Shiny user's personal dir
 git clone https://github.com/rstudio/shiny.git
