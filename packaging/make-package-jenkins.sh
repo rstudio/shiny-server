c1c54cf5ee3051f90b86f3593cd89abc626c6efa
Joe Cheng <joe@rstudio.com>
2016-10-12 17:12:16 -0700

    Attempt to get build working on Jenkins
    Build machines need to be convinced to use gcc-4.8 toolchain, and
    need to alias the python command too

---
+++ packaging/make-package-jenkins.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This is needed for CentOS5/6 Jenkins workers to bootstrap the gcc-4.8 toolchain.
+
+cd "$(dirname $0)"
+
+if (which scl && scl -l | grep -q devtoolset-2);
+then
+	scl enable devtoolset-2 ./make-package.sh "$@"
+else
+	CC=gcc-4.8 CXX=g++-4.8 ./make-package.sh "$@"
+fi
+
