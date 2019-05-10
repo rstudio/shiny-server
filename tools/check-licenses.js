1a31df3658cff3248c46c60de24f916bd6cd6278
Joe Cheng <joe@rstudio.com>
2019-05-10 15:39:10 -0700

    Add "Apache License, Version 2.0" to list of accepted licenses

--- tools/check-licenses.js
+++ tools/check-licenses.js
@@ -61,7 +61,7 @@ function getPackageJson(basedir) {
 }
 
 function isApprovedLicense(license) {
-  return /^MIT|BSD|BSD-3-Clause|ISC|Apache[- ]2.0|WTFPL|Public Domain|MPL[- ]2.0|zlib|Unlicense$/.test(license);
+  return /^MIT|BSD|BSD-3-Clause|ISC|Apache[- ]2.0|Apache License, Version 2.0|WTFPL|Public Domain|MPL[- ]2.0|zlib|Unlicense$/.test(license);
 }
 
 function getLicenses(path) {
