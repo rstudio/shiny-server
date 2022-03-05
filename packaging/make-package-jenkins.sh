ee8c67b8824417fb43f871d33a8bcf9967c60f03
Joe Cheng <joe@rstudio.com>
2022-03-04 20:41:20 -0800

    Remove unnecessary reset
    With the container-based jenkins workflow, there should be no need for
    this

--- packaging/make-package-jenkins.sh
+++ packaging/make-package-jenkins.sh
@@ -6,13 +6,6 @@ set -x
 # This is needed for CentOS5/6 Jenkins workers to bootstrap a newer gcc toolchain.
 cd "$(dirname $0)"
 
-# Repo checkout directories are re-used by Jenkins workers, and so a
-# $PROJECT_DIR/packaging/build/CMakeCache.txt might be hanging around from a
-# previous build. This cache file is platform-specific, and the build that
-# generated it may have been on a different platform. In order to build reliably
-# we must first blow it and any other files not in the repo away.
-git reset --hard && git clean -ffdx
-
 if (which scl && scl -l | grep -q devtoolset-11);
 then
 	scl enable devtoolset-11 ./make-package.sh
